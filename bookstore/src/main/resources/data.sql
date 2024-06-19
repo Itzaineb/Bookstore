19dea6512763db9ce2398a8d7e35782d4e8eec8d
Itzaineb <138628081+Itzaineb@users.noreply.github.com>
2024-06-19 15:26:26 +0100

    Add files via upload

---
+++ bookstore/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+-- data.sql
+INSERT INTO book (title, author, isbn) VALUES ('The Great Gatsby', 'F. Scott Fitzgerald', '9780743273565');
+INSERT INTO book (title, author, isbn) VALUES ('1984', 'George Orwell', '9780451524935');
+INSERT INTO book (title, author, isbn) VALUES ('To Kill a Mockingbird', 'Harper Lee', '9780061120084');
